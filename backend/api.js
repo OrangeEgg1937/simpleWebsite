a2e6f8eb8e6f891d6a2a42ae279530fcacefc77d
OrangeEgg1937 <48792791+OrangeEgg1937@users.noreply.github.com>
2023-12-13 16:33:01 +0000

    backend api

---
+++ backend/api.js
@@ -0,0 +1,27 @@
+// Return the request with the response
+module.exports = app => {
+const express = require('express');
+const api = express.Router();
+
+// For user collection
+const user = require("./mongodb/user_function.js");
+
+// Define the routes for the API
+
+// Definethe API route for getting a single user
+api.get('/users', (req, res) => {res.json({ message: "Return a single user info: " + req.query['id'] });});
+
+// Define the API route for getting all users
+api.all('/users/all', user.getAllUsers);
+
+// Definethe API to check the user is exist or not
+api.get('/users/exist', (req, res) => {res.json({ message: "Return true if user exist: " + req.query['id'] });});
+
+// If api is not defined, return 404
+api.get('/*', (req, res) => {
+    res.json({ message: "404 not found." });
+});
+
+// Define the subdomain for calling api
+app.use("/api", api);
+}
