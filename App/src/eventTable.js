2c78d1994923e80dca393d17081a0e06487447e5
tomk2002 <134872842+tomk2002@users.noreply.github.com>
2023-12-15 11:36:09 +0000

    EventTable update

--- App/src/eventTable.js
+++ App/src/eventTable.js
@@ -1,15 +1,14 @@
-import ReactDOM from "react-dom/client";
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { MDBDataTable } from 'mdbreact';
-import {Table, Form} from "react-bootstrap";
+import { Table, Form } from 'react-bootstrap';
 import {
   BrowserRouter,
   Routes,
   Route,
   Link,
 } from 'react-router-dom';
-import "./eventTable.css";
+import './eventTable.css';
 
 const EventTable = () => {
   const [data, setData] = useState({
@@ -43,77 +42,58 @@ const EventTable = () => {
         label: 'Price',
         field: 'price',
         sort: 'asc',
-      }
+      },
     ],
-    rows: [
-      {
-        title: 'Event 1',
-        venueId: 1,
-        time: '2023-12-15 10:00 AM',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-        organisation: 'Organization 1',
-        price: 10.99,
-      },
-      {
-        title: 'Event 2',
-        venueId: 2,
-        time: '2023-12-16 2:00 PM',
-        description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        organisation: 'Organization 2',
-        price: 15.99,
-      },
-
-    ]
+    rows: [],
   });
 
   const [maxPrice, setMaxPrice] = useState('');
 
-  /*
   useEffect(() => {
-    axios.get('http://localhost:8080/api/event/all')
+    axios.get('https://crispy-space-computing-machine-v6gwxgpqv5r2x49g-8080.app.github.dev/api/events/find/by10loc')
       .then((response) => {
-        const eventData = response.data;
-        const updatedRows = eventData.map((event) => ({
+        const eventList = response.data;
+        console.log(eventList);
+        const updatedRows = eventList.map((event) => ({
           title: event.title,
-          venueId: event.venueId,
-          time: event.time,
-          description: event.description,
-          organisation: event.organisation,
-          price: event.price
+          venueId: event.venueid,
+          time: event.progtime,
+          description: event.desce,
+          organisation: event.presenterorge,
+          price: event.price,
         }));
 
         setData((prevData) => ({
           ...prevData,
-          rows: updatedRows
+          rows: updatedRows,
         }));
+      })
+      .catch((error) => {
+        console.error('Error fetching event data:', error);
       });
   }, []);
-  */
 
   const handleFilter = () => {
     const filteredRows = data.rows.filter((row) => row.price <= maxPrice);
 
     setData((prevData) => ({
       ...prevData,
-      rows: filteredRows
+      rows: filteredRows,
     }));
   };
 
   return (
     <div>
-      <input type="number" placeholder="Max Price" value={maxPrice} onChange={(e) => setMaxPrice(e.target.value)} />
-      <button onClick={handleFilter}>Filter</button>
-      <MDBDataTable
-        striped
-        bordered
-        data={data}
-        searchable
-        filter="price"
+      <input
+        type="number"
+        placeholder="Max Price"
+        value={maxPrice}
+        onChange={(e) => setMaxPrice(e.target.value)}
       />
+      <button onClick={handleFilter}>Filter</button>
+      <MDBDataTable striped bordered data={data} searchable filter="price" />
     </div>
   );
 };
-
-
 
 export default EventTable;
